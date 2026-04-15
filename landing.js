445494b7043a99d23554d19f5f003034c6988ba7
mo-tunn <mo-tunn@users.noreply.github.com>
2026-04-15 15:59:58 +0000

    deploy: bb82f81392527caa2be32e9674024e66064aadbb

--- landing.js
+++ landing.js
@@ -55,7 +55,7 @@ const modules = [
 const GITHUB_REPO_URL = "https://github.com/mo-tunn/OpenGuider";
 const GITHUB_LATEST_RELEASE_URL = `${GITHUB_REPO_URL}/releases/latest`;
 const downloads = [
-  { os: "Windows", href: `${GITHUB_LATEST_RELEASE_URL}/download/OpenGuider-windows-latest.exe` },
+  { os: "Windows", href: `${GITHUB_LATEST_RELEASE_URL}/download/OpenGuider-windows-latest.zip` },
   { os: "MacOS", href: `${GITHUB_LATEST_RELEASE_URL}/download/OpenGuider-macos-latest.zip` },
   { os: "Linux", href: `${GITHUB_LATEST_RELEASE_URL}/download/OpenGuider-linux-latest.zip` }
 ];
